9c1819cd88b8f6b5c43d23beb9bd91e80ab06a8c
Saloon-App <hasti.hajipara@gmail.com>
2023-10-11 00:17:03 +0530

    treatment_screen

--- lib/customer/screen/treatment_screen.dart
+++ lib/customer/screen/treatment_screen.dart
@@ -1,58 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:salonsync/customer/model/treatment_card.dart';
 
-class ServicesScreen extends StatefulWidget {
-  const ServicesScreen({super.key});
+class TreatmentScreen extends StatefulWidget {
+  const TreatmentScreen({Key? key});
 
   @override
-  State<ServicesScreen> createState() => _ServicesScreenState();
+  State<TreatmentScreen> createState() => _TreatmentScreenState();
 }
 
-class _ServicesScreenState extends State<ServicesScreen> {
+class _TreatmentScreenState extends State<TreatmentScreen> {
+  List<Widget> cardList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _generateCards();
+  }
+
+  void _generateCards() {
+    int numCards = 7;
+
+    for (int i = 1; i <= numCards; i += 2) {
+      String image = 'assets/images/treatment-2.jpeg';
+      String treatmentName1 = 'Treatment $i';
+      double price1 = 400;
+
+      String treatmentName2 = 'Treatment ${i + 1}';
+      double price2 = 500;
+
+      if (numCards % 2 != 0 && i == numCards) {
+        cardList.add(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TreatmentCard(
+                    image: image,
+                    treatmentName: treatmentName1,
+                    price: price1,
+                  ).buildCardWidget(),
+                ),
+                SizedBox(width: 8.0),
+                Expanded(
+                  child: Card(),
+                ),
+              ],
+            ),
+          ),
+        );
+      } else {
+        cardList.add(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TreatmentCard(
+                    image: image,
+                    treatmentName: treatmentName1,
+                    price: price1,
+                  ).buildCardWidget(),
+                ),
+                SizedBox(width: 8.0),
+                Expanded(
+                  child: TreatmentCard(
+                    image: image,
+                    treatmentName: treatmentName2,
+                    price: price2,
+                  ).buildCardWidget(),
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<Widget> cardWidgets = [];
-    for (int i = 0; i < 20; i++) {
-      TreatmentCard card = TreatmentCard(
-        image: 'assets/images/treatment-2.jpeg',
-        treatmentName: 'Treatment $i',
-        details: 'Details $i',
-        price: 100,
-      );
-
-      cardWidgets.add(card.buildCardWidget(context));
-    }
-
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("Salon-List"),
-        leading: Icon(Icons.menu),
-        backgroundColor: Color.fromARGB(255, 78, 75, 75),
-        // elevation: 10.0,// for shadow effect
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: IconButton(
-              icon: Icon(Icons.more_vert),
-              onPressed: () {},
-            ),
-          )
-        ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Treatment Screen'),
+        ),
+        body: ListView(
+          children: cardList,
+        ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              children: cardWidgets,
-            ),
-          )
-        ],
-      ),
-    ));
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ServicesScreen(),
+      home: TreatmentScreen(),
     );
   }
 }
